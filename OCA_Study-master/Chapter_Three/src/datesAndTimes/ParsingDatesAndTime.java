643538c96a42ccc243b01efcefe2ad4dc975e40f
Viktoriia378 <victoria.epifanova.arkadievna@gmail.com>
2025-08-14 14:00:20 +0200

    update

--- OCA_Study-master/Chapter_Three/src/datesAndTimes/ParsingDatesAndTime.java
+++ OCA_Study-master/Chapter_Three/src/datesAndTimes/ParsingDatesAndTime.java
@@ -1,4 +1,18 @@
 package datesAndTimes;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class ParsingDatesAndTime {
+    public static void main(String[] args) {
+
+        //Just like the format() method, the parse() method takes a formatter as well. If you don’t specify one, it uses the default for that type.
+
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("MM dd yyyy");
+        LocalDate date = LocalDate.parse("01 02 2015", f);
+        LocalTime time = LocalTime.parse("11:22");
+        System.out.println(date);
+        System.out.println(time);
+    }
 }
